fbfa6eea1230d9d0067f3241e8e3eefaf6678c55
Evgeny Pilyugin <puls2007@rambler.ru>
2020-08-04 20:10:25 +0500

    lesson 01

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP базовый курс</title>
+    <style>
+        .lesson {
+            border: 1px solid #209bbf;
+            padding: 10px;
+        }
+
+        h5 {
+            padding-top: 10px;
+            border-top: 1px solid #80d9e8;
+        }
+    </style>
+</head>
+<body>
+<h1>Базовый курс PHP</h1>
+    <div class="container">
+        <div class="lesson">
+            <h3>Lesson 1</h3>
+            <?php require_once('./less/less1.php') ?> <!-- Lesson 1 -->
+        </div>
+    </div>
+</body>
+</html>
